c623145d95ef4cf701a00413fe774d5e29c738ea
Jason Landini <123970616+landinjm@users.noreply.github.com>
2025-11-20 19:02:17 -0800

    Updating the python wrapper package (#39)
    * updatin the bash scripts
    
    * okie
    
    * deleting old one
    
    * silly recurvsive stuff
    
    * python precommti & format
    
    * deleting draft solver
    
    * some format stuff
    
    * fixed merge issue
    
    * using collect here
    
    * sorting

---
+++ packages/PyJuliaMPBSolver/test.py
@@ -0,0 +1,15 @@
+from PyJuliaMPBSolver import JuliaMPBSolver
+
+print("precompiling & solving...")
+solver = JuliaMPBSolver()
+x, c0, cp, cm = solver.mpbpsolve()
+print(x)
+print(c0)
+print(cm)
+print(cp)
+
+x, c0, cp, cm = solver.icmpbpsolve()
+print(x)
+print(c0)
+print(cm)
+print(cp)
